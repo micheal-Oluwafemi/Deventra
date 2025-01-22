fcfde10ea2b577cfea136bc67e8d04a06355af7f
micheal Oluwafemi <raytech401@gmail.com>
2025-01-22 09:43:53 +0100

    Upated

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
   return (
-    <div className="mx-auto flex max-w-5xl justify-between border-b border-gray-600 px-3 py-2">
-      <div className="cursor-pointer font-youth text-2xl font-semibold text-accent transition-colors hover:text-accent-hover">
+    <div className="mx-auto flex max-w-5xl items-center justify-between border-b border-gray-600 px-3 py-2">
+      <div className="cursor-pointer font-youth text-2xl font-semibold text-accent transition-colors hover:text-accent-hover md:text-2xl">
         Deventra
       </div>
 
@@ -12,7 +12,7 @@ const Navbar = () => {
           Connect your wallet
         </Button>
 
-        <div>
+        <div className="hidden">
           <img src="/src/assets/header/avatar.svg" alt="" />
         </div>
       </div>
